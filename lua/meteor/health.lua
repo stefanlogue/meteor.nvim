5dea9b07e5d98ea4e21c250d7d9d0c96448ed865
Stefan Logue <stefanlogue@icloud.com>
2024-11-21 21:54:17 +0000

    feat(health): print version

--- lua/meteor/health.lua
+++ lua/meteor/health.lua
@@ -9,6 +9,15 @@ M.check = function()
   end
 
   vim.health.ok("meteor is installed")
+
+  local results = vim.system({ "meteor", "--version" }, { text = true }):wait()
+
+  if results.code ~= 0 then
+    vim.health.issues("unable to get meteor version", results.stderr)
+    return
+  end
+
+  vim.health.info("meteor version: " .. results.stdout)
 end
 
 return M
