c433dd024c08f669788c34a49c7828e1b913ea22
Stefan Logue <stefanlogue@icloud.com>
2024-11-22 12:23:38 +0000

    test(meteor): more test

--- tests/meteor/meteor_spec.lua
+++ tests/meteor/meteor_spec.lua
@@ -1,18 +1,18 @@
-local plugin = require("meteor")
+local spy = require("luassert.spy")
+local meteor = require("meteor")
 
 describe("meteor", function()
   describe("show", function()
-    it("creates a new buffer and window", function()
-      local bufcount = #vim.api.nvim_list_bufs()
-      local wincount = #vim.api.nvim_list_wins()
+    it("creates a new scratch buffer", function()
+      local nvim_create_buf_spy = spy.on(vim.api, "nvim_create_buf")
+      local buf_count = #vim.api.nvim_list_bufs()
 
-      plugin.show()
+      meteor.show()
 
-      local newbufcount = #vim.api.nvim_list_bufs()
-      local newwincount = #vim.api.nvim_list_wins()
+      local new_buf_count = #vim.api.nvim_list_bufs()
 
-      assert.equals(bufcount + 1, newbufcount, "buffer count increased by 1")
-      assert.equals(wincount + 1, newwincount, "window count increased by 1")
+      assert.equals(buf_count + 1, new_buf_count, "buffer count increased by 1")
+      assert.spy(nvim_create_buf_spy).was_called_with(false, true)
     end)
   end)
 end)
